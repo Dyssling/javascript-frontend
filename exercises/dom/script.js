f3757312cf344cfce18b054bec012a22d6144d7e
Dyssling <olle.lundgarden@hotmail.com>
2023-10-18 10:30:01 +0200

    exercise 3

---
+++ exercises/dom/script.js
@@ -0,0 +1,7 @@
+function changeText(){
+    document.querySelector("#text").innerHTML = "Nu har texten ändrats.";
+}
+
+function addObject(){
+    document.querySelector("#lista").innerHTML += "<li>Ännu ett objekt</li>";
+}
