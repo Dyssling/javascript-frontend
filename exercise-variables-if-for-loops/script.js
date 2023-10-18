5b1dec088ee87d729ea8ed908f3b40f92b50481e
Dyssling <olle.lundgarden@hotmail.com>
2023-10-18 09:09:16 +0200

    first exercise

---
+++ exercise-variables-if-for-loops/script.js
@@ -0,0 +1,13 @@
+let name = "Olle";
+let age = 22;
+let student = true;
+
+if (student == true){
+    console.log(`Hej ${name} vid ${age} års ålder, och välkommen till vår skola!`);
+}else{
+    console.log(`Hej ${name} vid ${age} års ålder, gå gärna med i vår skola!`);
+}
+
+for(x = 1; x <= 10; x++){
+    console.log(x);
+}
